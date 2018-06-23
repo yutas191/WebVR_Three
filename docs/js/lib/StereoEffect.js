eb50dfa68078e9954481d16e277c3e18f13de8ec
yutas191 <yutas191@gmail.com>
2018-06-23 16:52:51 +0900

    Update WebVR_Three

--- docs/js/lib/StereoEffect.js
+++ docs/js/lib/StereoEffect.js
@@ -67,10 +67,10 @@ THREE.StereoEffect = function ( renderer ) {
 		_top = _halfFocalHeight * _ndfl;
 		_bottom = -_top;
 		_innerFactor = ( _halfFocalWidth + this.separation / 2.0 ) / ( _halfFocalWidth * 2.0 );
-		_outerFactor = 1.0 - _innerFactor;
+		_outerFactor = 1.2 - _innerFactor;
 
-		_outer = _halfFocalWidth * 2.2 * _ndfl * _outerFactor;
-		_inner = _halfFocalWidth * 1.6 * _ndfl * _innerFactor;
+		_outer = _halfFocalWidth * 2.0 * _ndfl * _outerFactor;
+		_inner = _halfFocalWidth * 2.0 * _ndfl * _innerFactor;
 
 		// left
 
