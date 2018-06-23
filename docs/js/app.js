0837286cb70083f9c7fb53f34dabe8c7d0a355a9
yutas191 <yutas191@gmail.com>
2018-06-23 16:03:16 +0900

    Update WebVR_Three

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,150 +1,40 @@
-var clock = new THREE.Clock();
-var container;
-var camera, scene, raycaster, renderer;
-var room;
-var isMouseDown = false;
-var INTERSECTED;
-var crosshair;
-init();
-animate();
-function init() {
-	container = document.createElement( 'div' );
-	document.body.appendChild( container );
-	var info = document.createElement( 'div' );
-	scene = new THREE.Scene();
-	scene.background = new THREE.Color( 0x505050 );
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 10 );
-	scene.add( camera );
-	crosshair = new THREE.Mesh(
-		new THREE.RingBufferGeometry( 0.02, 0.04, 32 ),
-		new THREE.MeshBasicMaterial( {
-			color: 0xffffff,
-			opacity: 0.5,
-			transparent: true
-		} )
-	);
-	crosshair.position.z = - 2;
-	camera.add( crosshair );
-	room = new THREE.Mesh(
-		new THREE.BoxBufferGeometry( 6, 6, 6, 8, 8, 8 ),
-		new THREE.MeshBasicMaterial( { color: 0x404040, wireframe: true } )
-	);
-	room.position.y = 3;
-	scene.add( room );
-	scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
-	var light = new THREE.DirectionalLight( 0xffffff );
-	light.position.set( 1, 1, 1 ).normalize();
-	scene.add( light );
-	var geometry = new THREE.BoxBufferGeometry( 0.15, 0.15, 0.15 );
-	for ( var i = 0; i < 200; i ++ ) {
-		var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
-		object.position.x = Math.random() * 4 - 2;
-		object.position.y = Math.random() * 4 - 2;
-		object.position.z = Math.random() * 4 - 2;
-		object.rotation.x = Math.random() * 2 * Math.PI;
-		object.rotation.y = Math.random() * 2 * Math.PI;
-		object.rotation.z = Math.random() * 2 * Math.PI;
-		object.scale.x = Math.random() + 0.5;
-		object.scale.y = Math.random() + 0.5;
-		object.scale.z = Math.random() + 0.5;
-		object.userData.velocity = new THREE.Vector3();
-		object.userData.velocity.x = Math.random() * 0.01 - 0.005;
-		object.userData.velocity.y = Math.random() * 0.01 - 0.005;
-		object.userData.velocity.z = Math.random() * 0.01 - 0.005;
-		room.add( object );
+class App {
+	constructor(scene) {
+		this.scene = scene;
 	}
-	raycaster = new THREE.Raycaster();
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.vr.enabled = true;
-	container.appendChild( renderer.domElement );
-	renderer.domElement.addEventListener( 'mousedown', onMouseDown, false );
-	renderer.domElement.addEventListener( 'mouseup', onMouseUp, false );
-	renderer.domElement.addEventListener( 'touchstart', onMouseDown, false );
-	renderer.domElement.addEventListener( 'touchend', onMouseUp, false );
-	window.addEventListener( 'resize', onWindowResize, false );
-	//
-	window.addEventListener( 'vrdisplaypointerrestricted', onPointerRestricted, false );
-	window.addEventListener( 'vrdisplaypointerunrestricted', onPointerUnrestricted, false );
-	document.body.appendChild( WEBVR.createButton( renderer ) );
-}
-function onMouseDown() {
-	isMouseDown = true;
-}
-function onMouseUp() {
-	isMouseDown = false;
-}
-function onPointerRestricted() {
-	var pointerLockElement = renderer.domElement;
-	if ( pointerLockElement && typeof(pointerLockElement.requestPointerLock) === 'function' ) {
-		pointerLockElement.requestPointerLock();
-	}
-}
-function onPointerUnrestricted() {
-	var currentPointerLockElement = document.pointerLockElement;
-	var expectedPointerLockElement = renderer.domElement;
-	if ( currentPointerLockElement && currentPointerLockElement === expectedPointerLockElement && typeof(document.exitPointerLock) === 'function' ) {
-		document.exitPointerLock();
-	}
-}
 
-function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-}
-//
-function animate() {
-	renderer.setAnimationLoop( render );
-}
-function render() {
-	var delta = clock.getDelta() * 60;
-	if ( isMouseDown === true ) {
-		var cube = room.children[ 0 ];
-		room.remove( cube );
-		cube.position.set( 0, 0, - 0.75 );
-		cube.position.applyQuaternion( camera.quaternion );
-		cube.userData.velocity.x = ( Math.random() - 0.5 ) * 0.02 * delta;
-		cube.userData.velocity.y = ( Math.random() - 0.5 ) * 0.02 * delta;
-		cube.userData.velocity.z = ( Math.random() * 0.01 - 0.05 ) * delta;
-		cube.userData.velocity.applyQuaternion( camera.quaternion );
-		room.add( cube );
+	init() {
+		var light = new THREE.DirectionalLight(0xFFFFFF);
+		light.position.set(2, 2, 0);
+		scene.add( light );
+
+		var ambientLight = new THREE.AmbientLight(0x888888);
+		scene.add( ambientLight );
+
+		this.meshCube = new THREE.Mesh();
+		var geometryCube = new THREE.BoxGeometry(10, 10, 10);
+		var materialCube = new THREE.MeshLambertMaterial( { color: 0x00ff88 } );
+		this.meshCube = new THREE.Mesh( geometryCube, materialCube );
+		this.meshCube.position.set(0, 0, 50);
+		this.scene.add( this.meshCube );
+
+		var meshSphere = new THREE.Mesh();
+		var loaderSphere = new THREE.TextureLoader();
+		var textureSphere = loaderSphere.load( './img/photo.jpg');
+		var materialSphere = new THREE.MeshBasicMaterial({ map:textureSphere, side:THREE.BackSide });
+		var geometrySphere = new THREE.SphereGeometry(1000,32,32);
+		meshSphere = new THREE.Mesh( geometrySphere, materialSphere );
+		meshSphere.position.set(0, 0, 0);
+
+		this.scene.add( meshSphere );
 	}
-	// find intersections
-	raycaster.setFromCamera( { x: 0, y: 0 }, camera );
-	var intersects = raycaster.intersectObjects( room.children );
-	if ( intersects.length > 0 ) {
-		if ( INTERSECTED != intersects[ 0 ].object ) {
-			if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-			INTERSECTED = intersects[ 0 ].object;
-			INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-			INTERSECTED.material.emissive.setHex( 0xff0000 );
-		}
-	} else {
-		if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-		INTERSECTED = undefined;
+
+	update(dt) {
+		this.meshCube.rotation.x += dt * 0.8
+		this.meshCube.rotation.z += dt * 0.2
 	}
-	// Keep cubes inside room
-	for ( var i = 0; i < room.children.length; i ++ ) {
-		var cube = room.children[ i ];
-		cube.userData.velocity.multiplyScalar( 1 - ( 0.001 * delta ) );
-		cube.position.add( cube.userData.velocity );
-		if ( cube.position.x < - 3 || cube.position.x > 3 ) {
-			cube.position.x = THREE.Math.clamp( cube.position.x, - 3, 3 );
-			cube.userData.velocity.x = - cube.userData.velocity.x;
-		}
-		if ( cube.position.y < - 3 || cube.position.y > 3 ) {
-			cube.position.y = THREE.Math.clamp( cube.position.y, - 3, 3 );
-			cube.userData.velocity.y = - cube.userData.velocity.y;
-		}
-		if ( cube.position.z < - 3 || cube.position.z > 3 ) {
-			cube.position.z = THREE.Math.clamp( cube.position.z, - 3, 3 );
-			cube.userData.velocity.z = - cube.userData.velocity.z;
-		}
-		cube.rotation.x += cube.userData.velocity.x * 2 * delta;
-		cube.rotation.y += cube.userData.velocity.y * 2 * delta;
-		cube.rotation.z += cube.userData.velocity.z * 2 * delta;
+
+	render(dt) {
+
 	}
-	renderer.render( scene, camera );
 }
---
+++ docs/js/main.js
@@ -0,0 +1,95 @@
+var camera, renderer;
+var effect, controls;
+var element, container;
+
+var clock = new THREE.Clock();
+var scene = new THREE.Scene();
+var app = new App(scene);
+
+init();
+animate();
+
+function init() {
+	renderer = new THREE.WebGLRenderer();
+	element = renderer.domElement;
+	container = document.getElementById('example');
+	container.appendChild(element);
+
+	effect = new THREE.StereoEffect(renderer);
+
+	camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 10000 );
+	camera.position.set(0, 0, 0);
+	scene.add(camera);
+
+	controls = new THREE.OrbitControls(camera, element);
+	controls.target.set(
+		camera.position.x,
+		camera.position.y,
+		camera.position.z+0.1
+	);
+	controls.noZoom = true;
+	controls.noPan = true;
+
+	function setOrientationControls(e) {
+		if (!e.alpha) {
+			return;
+		}
+
+		controls = new THREE.DeviceOrientationControls(camera, true);
+		controls.connect();
+		controls.update();
+
+		element.addEventListener('click', fullscreen, false);
+
+		window.removeEventListener('deviceorientation', setOrientationControls, true);
+	}
+	window.addEventListener('deviceorientation', setOrientationControls, true);
+	window.addEventListener('resize', resize, false);
+	setTimeout(resize, 1);
+	app.init();
+}
+
+function resize() {
+	var width = container.offsetWidth;
+	var height = container.offsetHeight;
+
+	camera.aspect = width / height;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(width, height);
+	effect.setSize(width, height);
+}
+
+function update(dt) {
+	resize();
+
+	camera.updateProjectionMatrix();
+
+	controls.update(dt);
+	app.update(dt);
+}
+
+function render(dt) {
+	app.render(dt);
+	requestAnimationFrame(render);
+	effect.render(scene, camera);
+}
+
+function animate(t) {
+	requestAnimationFrame(animate);
+
+	update(clock.getDelta());
+	render(clock.getDelta());
+}
+
+function fullscreen() {
+	if (container.requestFullscreen) {
+		container.requestFullscreen();
+	} else if (container.msRequestFullscreen) {
+		container.msRequestFullscreen();
+	} else if (container.mozRequestFullScreen) {
+		container.mozRequestFullScreen();
+	} else if (container.webkitRequestFullscreen) {
+		container.webkitRequestFullscreen();
+	}
+}
